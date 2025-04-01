65d10d88f41e4335104ab1e4892a40bc0ecbe479
RomainPastureau <9433716+RomainPastureau@users.noreply.github.com>
2025-04-01 18:28:47 +0200

    Update e-mail address

--- setup.py
+++ setup.py
@@ -4,9 +4,3 @@ setup(name='find_delay',
       version='2.16',
       packages=find_packages(exclude=["demos"]),
       include_package_data=True)
-      # exclude_package_data={"demos": ["au_revoir.wav",
-      #                                 "i_have_a_dream_excerpt.wav",
-      #                                 "i_have_a_dream_excerpt2.wav",
-      #                                 "i_have_a_dream_excerpt_end.wav",
-      #                                 "i_have_a_dream_full_without_end.wav",
-      #                                 "i_have_a_dream_full_without_end_+800ms.wav"]})
